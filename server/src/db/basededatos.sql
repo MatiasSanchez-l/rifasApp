f5bf3aaad4243133ede7d3681573915040907142
Sanchez Matias <lomatiasleandro@gmail.com>
2021-01-09 17:36:19 -0300

    cambios en la vdd

--- server/src/db/basededatos.sql
+++ server/src/db/basededatos.sql
@@ -15,12 +15,21 @@ CREATE TABLE IF NOT EXISTS cliente (
     contrasenia VARCHAR ( 50 ) NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS compra(
+    compra_id SERIAL PRIMARY KEY,
+    monto INT NOT NULL,
+    fecha DATE NOT NULL DEFAULT CURRENT_DATE
+);
+
 CREATE TABLE IF NOT EXISTS rifa(
     rifa_id SERIAL PRIMARY KEY,
     disponible BOOLEAN NOT NULL,
     cliente_id INT,
+    compra_id INT,
     FOREIGN KEY (cliente_id)
-    REFERENCES usuario(cliente_id)
+    REFERENCES cliente(cliente_id),
+    FOREIGN KEY (compra_id)
+    REFERENCES compra(compra_id)
 );
 
 INSERT INTO usuario(nombre_usuario, contrasenia)

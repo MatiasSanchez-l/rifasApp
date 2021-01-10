087f5187e9e694339f7c64081548da71066a6c45
Sanchez Matias <lomatiasleandro@gmail.com>
2021-01-10 16:50:31 -0300

    se creo codigo para comprar rifas

--- server/src/controllers/rifas.controller.js
+++ server/src/controllers/rifas.controller.js
@@ -22,7 +22,7 @@ rifasCtrl.crear_rifas = async (req, res) => {
     let resultado  = "";
     for (let i = 0; i < cantidad_rifas; i++) {
         resultado  = await db.query(
-        "INSERT INTO rifa(disponible) VALUES(FALSE) RETURNING *"
+        "INSERT INTO rifa(disponible) VALUES(TRUE) RETURNING *"
       );
     }
 
@@ -37,5 +37,38 @@ rifasCtrl.crear_rifas = async (req, res) => {
     console.error(e.message);
   }
 };
+
+rifasCtrl.comprar_rifas = async (req, res) => {
+  try {
+    // cantidad de rifas a comprar
+    const cantidad_rifas_comprar = req.params.cantidad;
+
+    //obtener rifas disponibles
+    const rifas_disponibles = await db.query("SELECT rifa_id FROM rifa WHERE disponible = TRUE");
+    const cantidad_rifas_disponibles = rifas_disponibles.rows.length;
+    
+    let rifas_compradas = [];
+
+    //comprar rifas aleatorias
+   for (let i = 0; i < cantidad_rifas_comprar; i++) {
+      const numero_ramdon = Math.floor(Math.random() * cantidad_rifas_disponibles);
+
+      const rifa_a_comprar = rifas_disponibles.rows[numero_ramdon].rifa_id;
+
+      rifas_compradas.push(rifa_a_comprar);
+      
+      //sacar disponibilida de rifa
+      await db.query("UPDATE rifa SET disponible = false::boolean WHERE rifa_id = $1;", [rifa_a_comprar]);
+    }
+    
+    res.status(200).json({
+      cantidad: cantidad_rifas_comprar,
+      rifas_compradas: rifas_compradas,
+    });
+  } catch (e) {
+    console.error(e.message);
+  }
+};
+
 
 module.exports = rifasCtrl;

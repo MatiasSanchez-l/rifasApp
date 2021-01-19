50ac83910753748ceadcf143c8911eafce2b544b
SantiFagliano <santifagliano@live.com.ar>
2021-01-19 01:43:40 -0300

    intente mostrar datos de las rifas en una tabla y no me salio, solo muestra el id

--- frontend/src/components/Admin.js
+++ frontend/src/components/Admin.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import swal from 'sweetalert'
+import swal from 'sweetalert';
+import DataTable from 'react-data-table-component';
+
+const columnas = [
+    {
+        name: "Id rifa",
+        selector: "rifa_id",
+        sortable: true
+    },
+    {
+        name:"Disponibilidad",
+        selector:"disponible",
+        sortable:true
+    },
+    {
+        name:"Cliente Id",
+        selector:"cliente_id",
+        sortable:true
+    },
+    {
+        name:"Numero de Compra",
+        selector:"compra_id",
+        sortable:true
+    }
+]
 
 export default class Admin extends Component {
 
     state = {
-        cantidadRifasAGenerar: ''
+        cantidadRifasAGenerar: '',
+        rifas: []
     }
-
+    async componentDidMount() {
+        const res = await axios.get('http://localhost:5000/rifas');
+        this.setState({
+            rifas: res.data.data.rifas
+        })
+    }
     generarRifas = async e => {
         e.preventDefault();
         if (this.state.cantidadRifasAGenerar === "" || this.state.cantidadRifasAGenerar === '0') {
@@ -19,6 +49,7 @@ export default class Admin extends Component {
         } else {
             const res = await axios.post('http://localhost:5000/rifas/crear/' + this.state.cantidadRifasAGenerar);
             if (res.status === 200) {
+                this.componentDidMount();
                 swal({
                     title: "Creadas Correctamente",
                     text: "Cantidad de rifas creadas " + this.state.cantidadRifasAGenerar,
@@ -39,13 +70,14 @@ export default class Admin extends Component {
             [e.target.name]: e.target.value
         });
     }
+    guardar
     render() {
         return (
             <div className="mt-3">
                 <h3 className="text-center">
                     Bienvenido a la Seccion Admin
                 </h3>
-                <div className="row p-2">
+                <div className="row p-2 mt-3">
                     <div className="col-12 col-md-4">
                         <h3><i className="fas fa-caret-right"></i> Generar Rifas</h3>
                     </div>
@@ -64,6 +96,22 @@ export default class Admin extends Component {
                                 <button type="submit" className="mt-3 btn btn-success">Generar Rifas</button>
                             </div>
                         </form>
+                    </div>
+                </div>
+                <div className="row p-2 mt-3">
+                    <div className="col-12">
+                        <h3><i className="fas fa-caret-right"></i> Rifas</h3>
+                    </div>
+                    <div className="col-12 table-responsive">
+                        <DataTable 
+                        columns={columnas}
+                        data={this.state.rifas}
+                        title="Informacion Sobre Las Rifas"
+                        pagination
+                        fixedHeader
+                        theme="dark"
+                        fixedHeaderScrollHeight="500px"
+                        />
                     </div>
                 </div>
             </div>

258e730510b2398558d6c98afee4ee1a0367c3f3
SantiFagliano <santifagliano@live.com.ar>
2021-01-31 23:08:58 -0300

    algunos cambios

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,15 +1,46 @@
 import React, { Component } from 'react';
+import './home.css';
 
 export default class Home extends Component {
 
     render() {
         return (
-            <div>
-                home
-                <div className="container">
-                    <label for="customRange3" className="form-label">Plata recaudada</label>
-                    <input type="range" className="form-range" min="0" max="400000" value="310000" id="customRange3" />
-                    <p className="text-center">$</p>
+            <div className="container mt-3">
+                <div className="row">
+                    <div className="col-12 col-md-6">
+                        <h3>Donativo</h3>
+                        <p className="lh-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Earum fuga dolorum aspernatur consequatur exercitationem?
+                        Blanditiis dolor sunt modi quam totam iure ipsa veritatis soluta at, vel voluptatibus eveniet, quod fugit.
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident quia, labore ducimus animi sapiente sed tempore laborum omnis ipsum, assumenda quo unde, optio laboriosam minus voluptas soluta! Quia, quidem minima.</p>
+                    </div>
+                    <div className="col-12 col-md-6">
+                        <iframe 
+                        title="video papa"
+                        width="560" height="315" 
+                        src="https://www.youtube.com/embed/vdE_cZgx_L0" 
+                        frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                        allowfullscreen
+                        className="videoHome"></iframe>
+                    </div>
+                </div>
+                <hr/>
+                <div>
+                    <h5 className="text-center">Premios a sortear</h5>
+                    <div className="row">
+                        <div className="col-12 col-md-4 text-center img-premio mb-3">
+                            <img className="img-fluid img-thumbnail"
+                            src="https://http2.mlstatic.com/tostadora-electrica-kanjihome-kjh-tm900sec01-900w-plateada-D_NQ_NP_600821-MLA43694173299_102020-O.webp" alt=""/>
+                        </div>
+                        <div className="col-12 col-md-4 text-center img-premio mb-3">
+                            <img className="img-fluid img-thumbnail img-2"
+                            src="https://http2.mlstatic.com/pava-electrica-kanji-2-lts-1500w-ccorte-mate-castelar-D_NQ_NP_630835-MLA44174088004_112020-O.webp" alt=""/>
+                        </div>
+                        <div className="col-12 col-md-4 text-center img-premio mb-3">
+                            <img className="img-fluid img-thumbnail img-3"
+                            src="https://http2.mlstatic.com/D_NQ_NP_2X_693190-MLA44170440500_112020-F.webp" alt=""/>
+                        </div>
+                    </div>
                 </div>
             </div>
         )

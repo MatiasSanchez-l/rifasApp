8de42d1cc2f07452f7f22f4354db2cd430a68e66
Sanchez Matias <lomatiasleandro@gmail.com>
2021-02-06 15:36:29 -0300

    se saco codigo sin usar en validinfo

--- server/src/middleware/validinfo.js
+++ server/src/middleware/validinfo.js
@@ -4,12 +4,6 @@ module.exports = function(req, res, next) {
     function validEmail(userEmail) {
       return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(userEmail);
     }
-  
-    if (req.path === "/usuarios/login") {
-      if (![nombre_usuario, contrasenia].every(Boolean)) {
-        return res.status(401).json("Falta completar campos.");
-      }
-    }
 
     if (!validEmail(email)) {
       return res.status(401).json("Email no es correcto.");

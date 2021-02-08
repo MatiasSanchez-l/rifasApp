1c3767eb845dfdaa8612e440a3e859fac2cdbc8f
Sanchez Matias <lomatiasleandro@gmail.com>
2021-02-08 16:52:09 -0300

    se agrego module mercadopago

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const mercadopago = require('mercadopago');
 const app = express();
 
 //middlewares
 app.use(express.json());
 app.use(cors());
+
+mercadopago.configure({
+  access_token: 'PROD_ACCESS_TOKEN'
+});
 
 //rutas
 app.use("/rifas", require("./routes/rifas"));

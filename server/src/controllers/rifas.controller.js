e3e82b9759172ee6cfc66247ad487a9929a25023
SantiFagliano <santifagliano@live.com.ar>
2021-02-15 18:15:53 -0300

    prueba de mercadopago

--- server/src/controllers/rifas.controller.js
+++ server/src/controllers/rifas.controller.js
@@ -346,9 +346,7 @@ rifasCtrl.notificacion = async (req, res) => {
       const cliente_email = pago.response.payer.email;
       const fecha = new Date();
       const estado = "aprobado";
-      const cantidad_rifas_comprar = pago.response.aditional_info.items.quantity;
-      const precio = pago.response.aditional_info.items.unit_price;
-      const monto = cantidad_rifas_comprar * precio;
+
 
       //registrar cliente
       const cliente_id_json = await db.query(

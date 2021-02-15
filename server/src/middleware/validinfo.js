a2a9c8dfe3e513161880a76ec9d3457a5a1c5d1e
SantiFagliano <santifagliano@live.com.ar>
2021-02-15 16:03:13 -0300

    prueba de mercadopago

--- server/src/middleware/validinfo.js
+++ server/src/middleware/validinfo.js
@@ -6,7 +6,7 @@ module.exports = function(req, res, next) {
     }
 
     if (!validEmail(email)) {
-      return res.status(200).json({error: "Email no es correcto."});
+      return res.status(200).json({errores: "Email no es correcto."});
     }
 
     if(isNaN(telefono)){

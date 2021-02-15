780c8212fe1cedb3ba021bf233655a80262552ad
SantiFagliano <santifagliano@live.com.ar>
2021-02-15 16:04:47 -0300

    prueba de mercadopago

--- server/src/middleware/validinfo.js
+++ server/src/middleware/validinfo.js
@@ -1,17 +1,17 @@
-module.exports = function(req, res, next) {
-    const { telefono, email } = req.body;
+module.exports = function (req, res, next) {
+  const { telefono, email } = req.body;
 
-    function validEmail(userEmail) {
-      return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(userEmail);
-    }
+  function validEmail(userEmail) {
+    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(userEmail);
+  }
 
-    if (!validEmail(email)) {
-      return res.status(200).json({errores: "Email no es correcto."});
-    }
+  if (!validEmail(email)) {
+    return res.status(200).json({ errores: { mensaje: "Email no es correcto." } });
+  }
 
-    if(isNaN(telefono)){
-      return res.status(401).json("Numero de telefono no es correcto.");
-    }
-  
-    next();
-  };
+  if (isNaN(telefono)) {
+    return res.status(401).json("Numero de telefono no es correcto.");
+  }
+
+  next();
+};

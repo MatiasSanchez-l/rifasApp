6fdf302153a00475ac7ae084f1a65cf8fb1ef26b
Sanchez Matias <lomatiasleandro@gmail.com>
2021-02-16 18:24:02 -0300

    se valida largo del telefono

--- server/src/middleware/validinfo.js
+++ server/src/middleware/validinfo.js
@@ -9,7 +9,7 @@ module.exports = function (req, res, next) {
     return res.status(200).json({ errores: [{ mensaje: "Email no es correcto." }] });
   }
 
-  if (isNaN(telefono)) {
+  if (isNaN(telefono) || telefono.toString().length > 10 || telefono.toString().length < 8) {
     return res.status(200).json({ errores: [{ mensaje: "Numero de telefono no es correcto." }] });
   }
 
